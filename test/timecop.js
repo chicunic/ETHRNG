dd63dfc3fbaa33bef0f8e04fa9a92225afa84853
cy92830 <xiaoyu8301@gmail.com>
2017-03-19 23:28:03 +0800

    格式处理

--- test/timecop.js
+++ test/timecop.js
@@ -1,11 +1,11 @@
 var Timecop = require('./helper/timecop');
 
-contract('Timecop fast forward', function(accounts) {
+contract('Timecop fast forward', function (accounts) {
 
   it("fast forward 3 blocks", (done) => {
     var current = web3.eth.blockNumber;
 
-    Timecop.ff(3).then((height)=>{
+    Timecop.ff(3).then((height) => {
       assert.equal(height - current, 3);
       done();
     });
@@ -14,7 +14,7 @@ contract('Timecop fast forward', function(accounts) {
   it("fast forward zero block", (done) => {
     var current = web3.eth.blockNumber;
 
-    Timecop.ff().then((height)=>{
+    Timecop.ff().then((height) => {
       assert.equal(height - current, 0);
       done();
     });
