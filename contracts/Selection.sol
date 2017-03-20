f73543768cb18b44a2c42b4f3bd075e58e67a0b7
cy92830 <xiaoyu8301@gmail.com>
2017-03-20 19:57:30 +0800

    修改参与者是否被选中的判断逻辑

--- contracts/Selection.sol
+++ contracts/Selection.sol
@@ -6,6 +6,7 @@ contract Selection{
         uint256 reward; // 奖金
         uint256 serialNumber; // 编号
         address addrParticipant; // 地址
+
     }
 
     uint256 blockHash; // 发起者区块的 Hash
@@ -13,7 +14,7 @@ contract Selection{
     uint256 numSelected; // 被选中的人数
     uint256 numCombination; // 组合数 C(n,m)
     uint256 selectedResult; // 选中的参与者排列
-    bool selected; // 某个用户是否被选中
+    bool[] public selected; // 某个用户是否被选中
 
     // 计算组合数
     function countCelected(uint256 numParticipant, uint256 numSelected)
@@ -44,10 +45,11 @@ contract Selection{
     // 判断是否被选中
     function whetherSelected(uint256 numSelected)
         returns (bool selected) {
-            uint b = selectedResult >>（numSelected - 1）& 1;
+            return selected[numSelected];
+            /*uint b = selectedResult >>（numSelected - 1）& 1;
             if(b == 1)
                 return true;
             else
-                return false;
+                return false;*/
         }
 }
