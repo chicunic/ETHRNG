36dd1e497c8795d525130ae9b3959033e0c1e184
cy92830 <xiaoyu8301@gmail.com>
2017-03-21 18:25:10 +0800

    修改注释

--- contracts/Selection.sol
+++ contracts/Selection.sol
@@ -28,11 +28,10 @@ contract Selection{
 
 	// 计算选中的参与者排列
 	function selectedCombination(uint256 numParticipant, uint256 numSelected, uint256 blockHash, uint256 numCombination)
-	returns(uint256) {
+		returns(uint256) {
 		uint256 newHash = blockHash % numCombination;
-		// begins
 		uint256 countNo = 0; // 当前组合序号
-		bool[] memory selected = new bool[](numParticipant); // 初始化为0
+		bool[] memory selected = new bool[](numParticipant); // 创建长度为numParticipant的bool型数组
 		// 将前m个数置1，作为初始条件
 		uint256 i = 0;
 		for (i = 0; i < numSelected; i++) {
@@ -42,7 +41,7 @@ contract Selection{
 		do {
 			for (i = 0; i < numParticipant; i++) {
 				selectedResult *= 2;
-				if (selected[i] == true) 
+				if (selected[i]) 
 					selectedResult += 1;
 			}
 			if (newHash == countNo) {
@@ -52,13 +51,13 @@ contract Selection{
 			selectedResult = 0; // 进行下一次组合前结果清0
 			bFind = false;
 			for (i = 0; i < numParticipant - 1; i++) {
-				if (selected[i] == true && selected[i + 1] == false) {
+				if (selected[i] && !selected[i + 1]) {
 					// 交换10为01
 					selected[i] = false;
 					selected[i + 1] = true;
 					bFind = true;
 					// 如果第一位为0，则将第i位置之前的1移到最左边，如为1则第i位置之前的1就在最左边，无需移动
-					if (selected[0] == false) {
+					if (!selected[0]) {
 						uint256 j =0;
 						for (uint256 k = 0; k < i; k++) {
 							if (selected[k]) {
@@ -78,8 +77,7 @@ contract Selection{
 
 	// 判断是否被选中
 	function whetherSelected(uint256 numSelected)
-	returns(bool) {
-		//return selected[numSelected];
+		returns(bool) {
 		uint256 b = selectedResult >>(numSelected - 1)& 1;
 		if(b == 1)
 			return true;
