1c735f66fe61d1179e266cffdb137a9698ea27c4
cy92830 <xiaoyu8301@gmail.com>
2017-04-16 00:47:45 +0800

    删除无用的合约模块

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-//var Selection = artifacts.require("./Selection.sol");
 var Randao = artifacts.require("./Randao.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(Selection);
   deployer.deploy(Randao);
 };
